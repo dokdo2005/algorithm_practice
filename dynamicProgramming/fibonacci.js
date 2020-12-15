2d9dba30e6b3d833e3cf73183ecc280ba41dc0d1
dokdo2005 <dokdo2005@gmail.com>
2020-12-15 15:19:27 +0900

    modify fibonacci

--- dynamicProgramming/fibonacci.js
+++ dynamicProgramming/fibonacci.js
@@ -1,17 +1,17 @@
 const fibonacci = (x) => {
     let d = [];
-    for (i = 0; i <= x; i++) {
+    for (i = 1; i <= x; i++) {
         d.push(0);
     }
     const fibo = (x) => {
         if (x === 1 || x === 2) {
             return 1;
         }
-        if (d[x] !== 0) {
-            return d[x];
+        if (d[x - 1] !== 0) {
+            return d[x - 1];
         }
-        d[x] = fibo(x - 1) + fibo(x - 2);
-        return d[x];
+        d[x - 1] = fibo(x - 1) + fibo(x - 2);
+        return d[x - 1];
     }
     return fibo(x);
 }
